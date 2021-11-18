19d84acb7aa86defc6a91f94d4298d2501d35d5b
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-11-18 15:25:09 +0200

    fix: listen for events in Tasks

--- src/tasks/pages/Tasks.tsx
+++ src/tasks/pages/Tasks.tsx
@@ -22,46 +22,58 @@ const TASKS = [
 ];
 
 function Tasks() {
-  return (
-    <div>
-        <div className="header">
-            <div className="container">
-                <TaskForm />
+    const onUpdate = (task: any) => {
+        console.log('updated', task);
+    };
+
+    const onRemove = (task: any) => {
+        console.log('removed', task);
+    };
+
+    return (
+        <div>
+            <div className="header">
+                <div className="container">
+                    <TaskForm />
+                </div>
+            </div>
+
+            <div className="content container">
+                <div  className="list">
+                    {TASKS.map(task => (
+                        <Task
+                            task={task}
+                            onUpdate={onUpdate}
+                            onRemove={() => onRemove(task)}
+                        />
+                    ))}
+                    {/* <div *ngIf="todos$ | async as todos; else loading">
+                        <div *ngIf="todos.length">
+                            <todos-todo-item
+                                *ngFor="let item of todos$ | async"
+                                [item]="item"
+                                (remove)="onRemoveTodo($event)"
+                                (update)="onUpdateTodo($event)">
+                            </todos-todo-item>
+                        </div>
+
+                        <div *ngIf="!todos.length" class="todos-list__empty">
+                            <p>
+                                No tasks here. You are either a great doer, or a great procrastinator.
+                            </p>
+                        </div>
+                    </div> */}
+
+                    {/* <ng-template #loading>
+                        <div class="todos-list__loading">
+                            <i class="fa fa-spinner fa-spin"></i>
+                            Loading your tasks..
+                        </div>
+                    </ng-template> */}
+                </div>
             </div>
         </div>
-
-        <div className="content container">
-            <div  className="list">
-                {TASKS.map(task => (
-                    <Task task={task} />
-                ))}
-                {/* <div *ngIf="todos$ | async as todos; else loading">
-                    <div *ngIf="todos.length">
-                        <todos-todo-item
-                            *ngFor="let item of todos$ | async"
-                            [item]="item"
-                            (remove)="onRemoveTodo($event)"
-                            (update)="onUpdateTodo($event)">
-                        </todos-todo-item>
-                    </div>
-
-                    <div *ngIf="!todos.length" class="todos-list__empty">
-                        <p>
-                            No tasks here. You are either a great doer, or a great procrastinator.
-                        </p>
-                    </div>
-                </div> */}
-
-                {/* <ng-template #loading>
-                    <div class="todos-list__loading">
-                        <i class="fa fa-spinner fa-spin"></i>
-                        Loading your tasks..
-                    </div>
-                </ng-template> */}
-            </div>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default Tasks;
