77c879b006789d64678a594936a05a78db212841
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-11-22 17:26:52 +0200

    feat: update task @ client

--- src/tasks/clients/tasks-client.ts
+++ src/tasks/clients/tasks-client.ts
@@ -19,3 +19,12 @@ export const createTask = async (description: string): Promise<any> => {
 
     return { ...task, id };
 };
+
+export const updateTask = async (id: string, { description, isCompleted }: { description?: string; isCompleted?: boolean; }): Promise<any> => {
+    const taskRef = firestore.doc(`tasks/${id}`);
+
+    return taskRef.update({
+        ...(description ? { description } : null),
+        ...(isCompleted ? { isCompleted } : null)
+    })
+};
