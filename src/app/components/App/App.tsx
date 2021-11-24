a6508b57d8f6ea277cda61e01e9ec658e2a85bae
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-11-24 13:36:40 +0200

    feat: add default path

--- src/app/components/App/App.tsx
+++ src/app/components/App/App.tsx
@@ -1,10 +1,10 @@
 import { Header } from '..';
 import { Login } from '../../../auth/pages';
 import { Tasks } from '../../../tasks/pages';
-import { Route, Routes } from 'react-router-dom';
 import { actions } from '../../../auth/store';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { RequireAuth, RequireUnauth } from '../../../auth/components';
 
 function App() {
@@ -19,6 +19,11 @@ function App() {
       <Header />
 
       <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/tasks" replace />}
+        />
+
         <Route
           path="/login"
           element={
