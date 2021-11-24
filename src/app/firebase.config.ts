2e713ca002ce8d299a8f707ea857697668917f21
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-11-24 13:09:54 +0200

    chore: refactor

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDtZu1sQBTB6T6J6Iog1R1WE6qHWGSdPUk",
-    authDomain: "todo-react-bb577.firebaseapp.com",
-    projectId: "todo-react-bb577",
-    storageBucket: "todo-react-bb577.appspot.com",
-    messagingSenderId: "202292379378",
-    appId: "1:202292379378:web:ca392aea9af1760def5a6b"
-};
