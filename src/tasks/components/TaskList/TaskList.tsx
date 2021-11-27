c0e9b298568503fec9972c0152c26c6fa3a7b023
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-11-28 00:31:16 +0200

    feat: animation on create/remove tasks

--- src/tasks/components/TaskList/TaskList.tsx
+++ src/tasks/components/TaskList/TaskList.tsx
@@ -1,7 +1,8 @@
 import { Button } from '../../../app/components';
 import { Task } from '..';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styles from './TaskList.module.css';
-import { useEffect } from 'react';
+import { createRef, useEffect } from 'react';
 import { actions, selectGetTasksLoading, selectGetTasksStatus, selectRemoveTaskLoading, selectTasks, selectUpdateTaskLoading } from '../../store';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -48,16 +49,37 @@ const TaskList: React.FC = () => {
                     </div>
                 }
 
-                {!getTasksLoading && getTasksStatus === 'success' && tasks.map((task: any) => (
-                    <Task
-                        task={task}
-                        updateLoading={updateTaskLoading[task.id]}
-                        removeLoading={removeTaskLoading[task.id]}
-                        key={task.id || task.description}
-                        onUpdate={onUpdate}
-                        onRemove={() => onRemove(task)}
-                    />
-                ))}
+                {!getTasksLoading && getTasksStatus === 'success' && 
+                    <TransitionGroup>
+                        {tasks.map((task: any) => {
+                            const ref = createRef<HTMLDivElement>();
+
+                            return (
+                                <CSSTransition
+                                    classNames={{
+                                        enter: styles['task-enter'],
+                                        enterActive: styles['task-enter-active'],
+                                        exit: styles['task-exit'],
+                                        exitActive: styles['task-exit-active'],
+                                    }}
+                                    timeout={300}
+                                    key={task.id || task.description}
+                                    nodeRef={ref}
+                                >
+                                    <div ref={ref}>
+                                        <Task
+                                            task={task}
+                                            updateLoading={updateTaskLoading[task.id]}
+                                            removeLoading={removeTaskLoading[task.id]}
+                                            onUpdate={onUpdate}
+                                            onRemove={() => onRemove(task)}
+                                        />
+                                    </div>
+                                </CSSTransition>
+                            );
+                        })}
+                    </TransitionGroup>
+                }
             </div>
         </div>
     );
